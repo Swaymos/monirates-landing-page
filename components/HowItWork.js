022c9cf98ff0d5d1ec33967fdb193cbd49ee1c85
Amos Matthew <theswayamos@gmail.com>
2022-01-24 21:29:57 +0100

    initial

--- components/HowItWork.js
+++ components/HowItWork.js
@@ -18,7 +18,7 @@ export default function HowItWork() {
                     <li className={style.col3}>
                             <Image src={currency} alt = "currency icon"/>
                             <h3>Select Currency</h3>
-                            <p>Buy or Sell any currency at your own rate at zero exchange fee</p>
+                            <p>Place a buy or sell request by selecting the currency you wish to exchange.</p>
                         <Link href="" passHref>
                         Select Currency
                         </Link>
@@ -26,7 +26,7 @@ export default function HowItWork() {
                     <li className={style.col3}>
                             <Image src={offers} alt = "currency offers"/>
                             <h3>View Offers</h3>
-                            <p>Buy or Sell any currency at your own rate at zero exchange fee</p>
+                            <p>Multiple offers already waiting for your request. Get started in seconds</p>
                         <Link href="" passHref>
                         View Offers
                         </Link>
@@ -34,7 +34,7 @@ export default function HowItWork() {
                     <li className={style.col3}>
                             <Image src={swap} alt = "currency swap"/>
                             <h3>Swap Currency</h3>
-                            <p>Buy or Sell any currency at your own rate at zero exchange fee</p>
+                            <p>Quickly swap any currency you have to the one you want.</p>
                         <Link href="" passHref>
                         Swap Currency
                         </Link>
